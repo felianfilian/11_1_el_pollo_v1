1f3e7f23d8ffb419fd540f7ad53fe9b7d481db7b
felianfilian <darianbellentor@outlook.de>
2023-06-29 21:20:03 +0200

    create new objects

--- js/chicken.class.js
+++ js/chicken.class.js
@@ -1,1 +1,1 @@
-class chicken {}
+class Chicken {}
--- js/game.js
+++ js/game.js
@@ -1,6 +1,8 @@
 let canvas;
 let ctx;
-let character = new Movable();
+let character = new Character();
+let enemies = [new Chicken(), new Chicken(), new Chicken()];
+let world = new World();
 
 function init() {
   canvas = document.getElementById("canvas");
