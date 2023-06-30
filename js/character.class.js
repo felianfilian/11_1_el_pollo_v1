55f5865fcf2cb439bc1e59b5d6bb445380702c3d
felianfilian <darianbellentor@outlook.de>
2023-06-30 23:04:56 +0200

    load image cache

--- js/character.class.js
+++ js/character.class.js
@@ -1,6 +1,17 @@
 class Character extends Movable {
+  ANIM_WALK = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImage = 0;
+
   constructor(x, y) {
     super().loadImage("img/2_character_pepe/1_idle/idle/I-1.png");
+    this.loadImages(this.ANIM_WALK);
     this.x = x;
     this.y = y;
     this.height = 300;
--- js/movable.class.js
+++ js/movable.class.js
@@ -4,10 +4,24 @@ class Movable {
   height = 100;
   width = 100;
   img;
+  imageChache = [];
 
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
+  }
+
+  /**
+   *
+   * @param (Array) arr = ['img/img01.jpg','img/img01.jpg', ...]
+   */
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageChache[path] = img;
+    });
   }
 
   moveRight() {}
