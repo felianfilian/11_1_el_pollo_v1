dd6e2929665726fb3a436552af766972bf1ec24e
felianfilian <darianbellentor@outlook.de>
2023-07-02 13:00:04 +0200

    set keys

--- js/game.js
+++ js/game.js
@@ -5,10 +5,42 @@ let keyboard = new Keyboard();
 
 function init() {
   canvas = document.getElementById("canvas");
-  world = new World(canvas);
+  world = new World(canvas, keyboard);
   console.log("Character: ", world.character);
 }
 
-window.addEventListener("keypress", (e) => {
-  console.log(e);
+window.addEventListener("keydown", (e) => {
+  if (e.keycode == 32) {
+    keyboard.SPACE = true;
+  }
+  if (e.keycode == 37) {
+    keyboard.LEFT = true;
+  }
+  if (e.keycode == 38) {
+    keyboard.UP = true;
+  }
+  if (e.keycode == 39) {
+    keyboard.RIGHT = true;
+  }
+  if (e.keycode == 40) {
+    keyboard.DOWN = true;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.keycode == 32) {
+    keyboard.SPACE = false;
+  }
+  if (e.keycode == 37) {
+    keyboard.LEFT = false;
+  }
+  if (e.keycode == 38) {
+    keyboard.UP = false;
+  }
+  if (e.keycode == 39) {
+    keyboard.RIGHT = false;
+  }
+  if (e.keycode == 40) {
+    keyboard.DOWN = false;
+  }
 });
