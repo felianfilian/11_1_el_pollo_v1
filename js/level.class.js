3e8a0bccc28eeb44e8a2ef47cf97e2bacb2bd67b
felianfilian <darianbellentor@outlook.de>
2023-07-03 22:56:21 +0200

    create level class

---
+++ js/level.class.js
@@ -0,0 +1,34 @@
+class Level {
+  enemies = [new Chicken(), new Chicken(), new Chicken()];
+  clouds = [new Cloud("img/5_background/layers/4_clouds/1.png")];
+  backgrounds = [
+    new Background("img/5_background/layers/air.png", -719, 0),
+    new Background("img/5_background/layers/3_third_layer/2.png", -719, -20),
+    new Background("img/5_background/layers/2_second_layer/2.png", -719, -10),
+    new Background("img/5_background/layers/1_first_layer/2.png", -719, 0),
+    new Background("img/5_background/layers/air.png", 0, 0),
+    new Background("img/5_background/layers/3_third_layer/1.png", 0, -20),
+    new Background("img/5_background/layers/2_second_layer/1.png", 0, -10),
+    new Background("img/5_background/layers/1_first_layer/1.png", 0, 0),
+    new Background("img/5_background/layers/air.png", 719, 0),
+    new Background("img/5_background/layers/3_third_layer/2.png", 719, -20),
+    new Background("img/5_background/layers/2_second_layer/2.png", 719, -10),
+    new Background("img/5_background/layers/1_first_layer/2.png", 719, 0),
+    new Background("img/5_background/layers/air.png", 719 * 2, 0),
+    new Background("img/5_background/layers/3_third_layer/1.png", 719 * 2, -20),
+    new Background(
+      "img/5_background/layers/2_second_layer/1.png",
+      719 * 2,
+      -10
+    ),
+    new Background("img/5_background/layers/1_first_layer/1.png", 719 * 2, 0),
+    new Background("img/5_background/layers/air.png", 719 * 3, 0),
+    new Background("img/5_background/layers/3_third_layer/2.png", 719 * 3, -20),
+    new Background(
+      "img/5_background/layers/2_second_layer/2.png",
+      719 * 3,
+      -10
+    ),
+    new Background("img/5_background/layers/1_first_layer/2.png", 719 * 3, 0),
+  ];
+}
