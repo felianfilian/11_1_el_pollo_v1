b02a4ff7a5b69f8133c4601b842de169191c3d91
felianfilian <darianbellentor@outlook.de>
2023-07-13 08:32:50 +0200

    drawable class

---
+++ js/drawable.class.js
@@ -0,0 +1,43 @@
+class Drawable {
+  img;
+  imageChache = [];
+  currentImage = 0;
+  deathImage = 0;
+
+  x = 120;
+  y = 340;
+  height = 100;
+  width = 100;
+
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  /**
+   *
+   * @param (Array) arr = ['img/img01.jpg','img/img01.jpg', ...]
+   */
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageChache[path] = img;
+    });
+  }
+
+  drawFrame(ctx) {
+    if (
+      this instanceof Character ||
+      this instanceof Chicken ||
+      this instanceof Endboss
+    ) {
+      ctx.beginPath();
+      ctx.lineWidth = "5";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
+    }
+  }
+}
--- js/movable.class.js
+++ js/movable.class.js
@@ -1,38 +1,12 @@
-class Movable {
-  x = 120;
-  y = 340;
-  height = 100;
-  width = 100;
+class Movable extends Drawable {
   speed = 0.4;
   speedY = 0;
   acceleration = 2.5;
   groundLevel = 140;
   energy = 100;
 
-  img;
-  imageChache = [];
-  currentImage = 0;
-  deathImage = 0;
   lookLeft = false;
   lastHit = 0;
-
-  loadImage(path) {
-    this.img = new Image();
-    this.img.src = path;
-  }
-
-  /**
-   *
-   * @param (Array) arr = ['img/img01.jpg','img/img01.jpg', ...]
-   */
-
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageChache[path] = img;
-    });
-  }
 
   moveRight() {
     this.x += this.speed;
@@ -77,22 +51,6 @@ class Movable {
 
   isGrounded() {
     return this.y >= this.groundLevel;
-  }
-
-  draw(ctx) {}
-
-  drawFrame(ctx) {
-    if (
-      this instanceof Character ||
-      this instanceof Chicken ||
-      this instanceof Endboss
-    ) {
-      ctx.beginPath();
-      ctx.lineWidth = "5";
-      ctx.strokeStyle = "blue";
-      ctx.rect(this.x, this.y, this.width, this.height);
-      ctx.stroke();
-    }
   }
 
   isColliding(obj) {
