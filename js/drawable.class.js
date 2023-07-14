583b8e28882334234cdd2fb4e98c406270b34a09
felianfilian <darianbellentor@outlook.de>
2023-07-14 11:18:13 +0200

    drawable test 02

--- js/drawable.class.js
+++ js/drawable.class.js
@@ -1,3 +1,22 @@
 class Drawable {
-  test;
+  img;
+  imageChache = [];
+  currentImage = 0;
+  deathImage = 0;
+
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+  /**
+   *
+   * @param (Array) arr = ['img/img01.jpg','img/img01.jpg', ...]
+   */
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageChache[path] = img;
+    });
+  }
 }
